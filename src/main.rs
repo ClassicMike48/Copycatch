5ce1be9b72de38630c7693905f092bc31f04b946
Classic_Mike48 <mbrudos48@gmail.com>
2026-06-29 10:04:29 -0500

    WIP: Adding support for cryptographic hashing for duplicate detection.

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ use std::{
     io,
     path::Path,
 };
+use image::DynamicImage;
+use sha2::{Digest, Sha256};
 
 // create a file walker that will visit all files in a tree
 //recursive function
@@ -12,12 +14,20 @@ fn print_file_name(file: &DirEntry) {
 }
 
 //TODO consider adding a cryptographic hash for exact file detection
-fn print_path(file: &DirEntry) {
+//utilize SHA-256 
+fn get_crypto_hash(image: &DynamicImage) -> String {
+    let data = image.as_bytes();
+    let hash = Sha256::digest(data);
+    hash.iter().map(|b| format!("{:02x}", b)).collect()
+}
+fn get_phash(file: &DirEntry) {
     let path = file.path();
     match image::open(&path) {
         Ok(image) => {
             let phash = imagehash::perceptual_hash(&image);
-            print!("{}", phash);
+            phash.to_string();
+            let sha = get_crypto_hash(&image);
+            println!("{}\n- phash: {}\n- sha256: {}", path.display(), phash, sha);
         }
         Err(_) => println!(
             "Error occurred while trying to read from {}",
@@ -55,9 +65,12 @@ fn main() {
     if args.len() < 2 {
         // No directory provided, use default
         println!("No path provided, using default directory: 'test'");
-        let _ = visit_directory(Path::new("test"), &print_file_name);
+        let _ = visit_directory(Path::new("test"), &get_phash);
     } else {
         let dir_path = &args[1];
         let _ = visit_directory(Path::new(dir_path), &print_file_name);
     }
 }
+//Hash outputs in lowercase hex
+// 50da58893c4db448c810d689fc9f4ef9e091d37f5aab4e383a1142e287e4f8bf
+// 000036dadbdbdbda
